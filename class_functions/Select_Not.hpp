81a3eef441a2c5407fe82f6a0dd9086e84d874e6
Jsam88 <jordansam@sbcglobal.net>
2021-05-03 01:42:03 -0700

    Modified Select_Not

--- class_functions/Select_Not.hpp
+++ class_functions/Select_Not.hpp
@@ -2,13 +2,35 @@
 #define __SELECT_NOT_HPP__
 
 #include "select.hpp"
+#include "spreadsheet.hpp"
 #include <iostream>
 
-class Select
+class Select_Not: public Select{
 
-class Select_Not: public select{
+protected:
+	Select *location1;
+	Select *location2;
+public:
 
-    
-}
+	Select_Not(Select* l1, Select* l2) {
+		location1 = l1;
+		location2 = l2;
+	}
+
+	~Select_Not() {
+		delete location1;
+		delete location2;
+	}
+
+	bool select(const Spreadsheet* sheet, int row) const{
+		if (location1->select(sheet,row) == true && location2->select(sheet, row) == true) {
+			return false;
+		}
+		else {
+			return true;
+		}
+   	}
+};
+
 
 #endif
