4419d52af7f1bcd05900eaf0a641e326cae954f7
Jsam88 <jordansam@sbcglobal.net>
2021-05-04 04:23:29 -0700

    Added more test cases

--- Select_Tests.hpp
+++ Select_Tests.hpp
@@ -53,7 +53,7 @@ TEST(SelectContainsTest, uppercaseTest) {
     EXPECT_EQ(test, "APPLE \n");
 }
 
-TEST(SelectContainsTest, ContainsTest) {
+TEST(SelectContainsTest, ContainsTest1) {
     Spreadsheet sheet;
     sheet.set_column_names({"Food"});
     sheet.add_row({"apple"});
@@ -70,6 +70,25 @@ TEST(SelectContainsTest, ContainsTest) {
     std::string test = ss.str();
     EXPECT_EQ(test, "mango \n");
 }
+
+TEST(SelectContainsTest, ContainsTest2) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Food"});
+    sheet.add_row({"apple"});
+    sheet.add_row({"apples"});
+    sheet.add_row({"APPLE"});
+    sheet.add_row({"snapple"});
+    sheet.add_row({"app"});
+    sheet.add_row({"mango"});
+
+    sheet.set_selection(new Select_Contains(&sheet, "Food", "yay"));
+
+    std::stringstream ss;
+    sheet.print_selection(ss);
+    std::string test = ss.str();
+    EXPECT_EQ(test, "");
+}
+
 
 TEST(SelectNotTest, NotTest1) {
     Spreadsheet sheet;
@@ -111,6 +130,28 @@ TEST(SelectNotTest, NotTest2) {
     EXPECT_EQ(test, "banana \n");
 }
 
+TEST(SelectNotTest, NotTest3) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Food"});
+    sheet.add_row({"apple"});
+    sheet.add_row({"apples"});
+    sheet.add_row({"APPLE"});
+    sheet.add_row({"banana"});
+    sheet.add_row({"bananas"});
+
+    sheet.set_selection(
+        new Select_And(
+                new Select_Contains(&sheet, "Food", ""),
+                new Select_Not(
+                        new Select_Contains(&sheet, "Food", "s"))));
+
+    std::stringstream ss;
+    sheet.print_selection(ss);
+    std::string test = ss.str();
+    EXPECT_EQ(test, "apple \nAPPLE \nbanana \n");
+}
+
+
 TEST(SelectAndTest, AndTest1) { //we want to get nothing
     Spreadsheet sheet;
     sheet.set_column_names({"Food"});
@@ -151,6 +192,27 @@ TEST(SelectAndTest, AndTest2) {
     EXPECT_EQ(test, "banana \nbananas \n");
 }
 
+TEST(SelectAndTest, AndTest3) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Food"});
+    sheet.add_row({"apple"});
+    sheet.add_row({"apples"});
+    sheet.add_row({"banana"});
+    sheet.add_row({"bananas"});
+
+    sheet.set_selection(
+        new Select_And(
+                new Select_Contains(&sheet, "Food", "banana"),
+		new Select_Not(
+                	new Select_Contains(&sheet, "Food", "apple"))));
+
+    std::stringstream ss;
+    sheet.print_selection(ss);
+    std::string test = ss.str();
+    EXPECT_EQ(test, "banana \nbananas \n");
+}
+
+
 TEST(SelectOrTest, OrTest1) {
     Spreadsheet sheet;
     sheet.set_column_names({"Food"});
@@ -186,6 +248,26 @@ TEST(SelectOrTest, OrTest2) {
 		new Select_Or(
                 	new Select_Contains(&sheet, "Food", "APPLE"),
 			new Select_Contains(&sheet, "Food", "apple"))));
+
+    std::stringstream ss;
+    sheet.print_selection(ss);
+    std::string test = ss.str();
+    EXPECT_EQ(test, "apple \napples \nAPPLE \nbanana \nbananas \n");
+}
+
+TEST(SelectOrTest, OrTest3) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Food"});
+    sheet.add_row({"apple"});
+    sheet.add_row({"apples"});
+    sheet.add_row({"APPLE"});
+    sheet.add_row({"banana"});
+    sheet.add_row({"bananas"});
+
+    sheet.set_selection(
+        new Select_Or(
+                new Select_Contains(&sheet, "Food", "beb"),
+                new Select_Contains(&sheet, "Food", "")));
 
     std::stringstream ss;
     sheet.print_selection(ss);
